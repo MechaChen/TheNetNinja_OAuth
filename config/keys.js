e913a05ba4b5e3350167a64dbdcdfc8114838857
Benson <tumlivein@gmail.com>
2019-11-10 23:35:05 +0800

    7. Securing Keys

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// add this file .gitignore
+
+module.exports = {
+    google: {
+        clientID: '989015200538-h0jq022gfhgohtqa4qokrsrjo44br1s5.apps.googleusercontent.com',
+        clientSecret: 'o3ls11r7LRilHCz9JRzw5hJn'
+    }
+};
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,11 +1,12 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./keys');
 
 passport.use(
     new GoogleStrategy({
         // options for the google strat
-        clientID: '989015200538-h0jq022gfhgohtqa4qokrsrjo44br1s5.apps.googleusercontent.com',
-        clientSecret: 'o3ls11r7LRilHCz9JRzw5hJn'
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret
     }), () => {
         // passport callback function
     }
