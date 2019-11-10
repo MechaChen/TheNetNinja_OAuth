0110d7473fde7d5df67f979dd50d899a785fe4d2
Benson <tumlivein@gmail.com>
2019-11-10 23:30:30 +0800

    6. Google Strategy Setup

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -4,7 +4,8 @@ const GoogleStrategy = require('passport-google-oauth20');
 passport.use(
     new GoogleStrategy({
         // options for the google strat
-
+        clientID: '989015200538-h0jq022gfhgohtqa4qokrsrjo44br1s5.apps.googleusercontent.com',
+        clientSecret: 'o3ls11r7LRilHCz9JRzw5hJn'
     }), () => {
         // passport callback function
     }
