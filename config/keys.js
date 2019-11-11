6c767b4f6ec62c937e82a689dd7e6ddd155ca864
Benson <tumlivein@gmail.com>
2019-11-11 23:35:50 +0800

    10. Creating a Database on mLab

--- config/keys.js
+++ config/keys.js
@@ -4,5 +4,8 @@ module.exports = {
     google: {
         clientID: '989015200538-h0jq022gfhgohtqa4qokrsrjo44br1s5.apps.googleusercontent.com',
         clientSecret: 'o3ls11r7LRilHCz9JRzw5hJn'
+    },
+    mongodb: {
+        dbURI: 'mongodb://bensonyue:YueGp60208@ds245018.mlab.com:45018/nn-oauth-test'
     }
 };
