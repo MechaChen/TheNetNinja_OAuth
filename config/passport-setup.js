92cad99d9ab88e4ce84c2d16e045629c20569b0b
Benson <tumlivein@gmail.com>
2019-11-11 23:17:44 +0800

    9. Passport Callback Function

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -8,7 +8,11 @@ passport.use(
         callbackURL: '/auth/google/redirect',
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret
-    }, () => {
+    }, (accessToken, refreshToken, profile, done) => {
         // passport callback function
+        console.log('accessToken:', accessToken);
+        console.log('refreshToken:', refreshToken);
+        console.log('profile', profile);
+        console.log('done:', done);
     })
 );
